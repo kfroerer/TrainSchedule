d99475886ea6c505069744c0381aa26b770d206b
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-11-12 18:32:44 -0600

    time functions not fully working

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+    var displayTime = function(){
+    var currentTime = moment().format("h:mm:ss a");
+    $("#currentTime").text("Current Time is " + currentTime);
+    }
+    setInterval(displayTime, 1000);
+
     var config = {
         apiKey: "AIzaSyCO83hdTWvoo7hakBLQk3HMe1EKig4Z--k",
         authDomain: "train-schedule-721b9.firebaseapp.com",
@@ -13,13 +19,13 @@ $(document).ready(function(){
     var database = firebase.database();
     
     var trainName = "";
-    var trackNum = "";
     var firstTrain= "";
     var frequency = "";
+    var destination = "";
         
     var trainData  = {
+        destination: destination,
         trainName: trainName,
-        trackNum: trackNum,
         firstTrain: firstTrain,
         frequency: frequency,
         dateAdded: firebase.database.ServerValue.TIMESTAMP
@@ -27,9 +33,8 @@ $(document).ready(function(){
 
     $("#submit-btn").on("click", function (){
         event.preventDefault();
-
+        destination = $("#destination").val().trim();
         trainName = $("#name").val().trim();
-        trackNum = $("#track").val().trim();
         firstTrain = $("#firstTrain").val().trim();
         frequency = $("#frequency").val().trim();
         console.log(trainName);
@@ -38,15 +43,25 @@ $(document).ready(function(){
     });
         
     database.ref().on("child_added", function(childsnapshot) {
-        console.log(childsnapshot.val());
-        
+        // console.log(childsnapshot.val());
+        destination = childsnapshot.val().destination;
         trainName = childsnapshot.val().trainName;
         trackNum = childsnapshot.val().trackNum;
         firstTrain = childsnapshot.val().firsTrain;
         frequency = childsnapshot.val().frequency;
 
-    var nextArrival =     
-    var minutesAway =     
+        var convFirstTrain = moment(firstTrain, "HH:mm").subtract(1, "years");
+        console.log("converted:  "+ convFirstTrain);
+        // var firstTrainconverted = moment().diff(convFirstTrain, "hh:mm");
+        var timeDiff = moment().diff(moment(convFirstTrain), "minutes");
+        console.log("Difference in time: " + timeDiff);
+        var timeRemain = timeDiff % frequency;
+        var minutesAway = frequency - timeRemain;
+        console.log("MINUTES TILL TRAIN: " + minutesAway);
+        var nextTrain = moment().add(minutesAway, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    
+
                
     });
     
